5ebb4294ef30d7e6926f0851d699d62d134bbb34
yasserkhan45 <khanyasser4599@gmail.com>
2021-03-27 18:09:34 +0530

    Added 4 more unit tests in author and authors

--- test.py
+++ test.py
@@ -7,6 +7,18 @@ except Exception as e:
 
 class FlaskTest(unittest.TestCase):
     
+    def test_authors_status(self):
+        tester = app.test_client(self)
+        response = tester.get("/authors")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    def test_author_status(self):
+        tester = app.test_client(self)
+        response = tester.get("/authors")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
     def test_authors(self):
         tester = app.test_client(self)
         response = tester.get("/authors")
@@ -18,6 +30,17 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/author")
         statuscode = response.status_code
         self.assertEqual(response.content_type, "application/json")
+    
+    def test_authors_data(self):
+        tester = app.test_client(self)
+        response = tester.get("/author")
+        self.assertTrue(b'message' in response.data)
+
+    def test_author_data(self):
+        tester = app.test_client(self)
+        response = tester.get("/author")
+        self.assertTrue(b'message' in response.data)
+    
 
 if __name__ == "__main__":
     unittest.main()
