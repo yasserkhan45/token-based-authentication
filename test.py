6f82a949022920750999fbde44c40dce1666e113
yasserkhan45 <khanyasser4599@gmail.com>
2021-03-30 21:48:01 +0530

    update

--- test.py
+++ test.py
@@ -22,13 +22,11 @@ class FlaskTest(unittest.TestCase):
     def test_authors(self):
         tester = app.test_client(self)
         response = tester.get("/authors")
-        statuscode = response.status_code
         self.assertEqual(response.content_type, "application/json")
     
     def test_author(self):
         tester = app.test_client(self)
         response = tester.get("/author")
-        statuscode = response.status_code
         self.assertEqual(response.content_type, "application/json")
     
     def test_authors_data(self):
